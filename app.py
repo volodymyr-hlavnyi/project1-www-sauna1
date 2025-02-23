f48b08753e3a57d5d57455ffd3d6d6c70e883b67
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2025-02-23 20:05:55 +0100

    02_add_menu_add_home_carousel_with_img

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/age-calculator', methods=["GET", "POST"])
-# def index():
-#     return render_template('age-calculator.html')
-
-
-#For base and page2 html
-
 
 @app.route('/')
-def base():
+def home():
+    return render_template('home.html', title='Home')
 
-    return render_template('home.html')
+
+@app.route('/angebote')
+def angebote():
+    return render_template('angebote.html', title='Angebote')
+
+
+@app.route('/preise')
+def preise():
+    return render_template('preise.html', title='Preise')
+
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html', title='Kontakt')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6200, debug=False)
+    app.run(host="0.0.0.0", port=6200, debug=True)
